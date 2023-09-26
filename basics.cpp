f453362c3807cb7fafcd0a0f49564eae5e1a6763
Arijit Roy <arijitroy042017@gmail.com>
2023-09-26 23:38:15 +0530

    initial commit

--- basics.cpp
+++ basics.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 int main()
 {
-    cout << "Hello World";
+    // cout << "Hello World";
+
+    // Taking input from user
+    int n;
+    cin >> n;
+    cout << "Entered Number is: " << n << endl;
+
+    // Statement
+    if (n > 0)
+    {
+        cout << "positive number.";
+    }
+    else if (n < 0)
+    {
+        cout << "Negative number.";
+    }
+    else
+    {
+        cout << "Zero.";
+    }
 }
